a77080007aab8a0894c21fc4a656218e58ee1860
reto1equipo3 <signupsigninequipo3@gmail.com>
2018-12-19 10:09:30 +0100

    FirstCommit

---
+++ EasyOrderAppServer/src/java/javafxserverside/rest/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains business logic tier classes and interfaces for
+ * user management application. This package is made up of an stateless EJB and
+ * its local interface.
+ */
+package javafxserverside.rest;
